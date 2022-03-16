00a216e3a552b7405359ab816ced0de52e27fca7
Iamtks11 <tusharkhorwal11@gmail.com>
2022-03-16 15:46:13 +0530

    add procfile

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const MongoUrl = 'mongodb://localhost/collaborate_db';
-mongoose.connect(MongoUrl);
+const atlasUrl = 'mongodb+srv://collaborate_se:collaborate1234@cluster0.vtouc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(atlasUrl);
 
 const db = mongoose.connection;
 
