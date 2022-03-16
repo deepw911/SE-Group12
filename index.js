62ac8d1a98caed770096e0e0c9aa147f27cc70ae
Iamtks11 <tusharkhorwal11@gmail.com>
2022-03-16 07:33:39 +0530

    Setup home router and controller, home page

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 
+//import router
+const routes = require('./routes/index');
+
 const PORT = 80;
 
+//view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
+//static folder
+app.use(express.static('./assets'));
+
+
+//route
+app.use('/', routes);
 
 
 
